635039ce0bf3baecceaa494dd80b3d8232ccfb5e
GChanathip <ch_chanathip@kkumail.com>
2024-08-20 22:47:58 +0700

    update patrol version

--- integration_test/interact_with_webview_test.dart
+++ integration_test/interact_with_webview_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:patrol/patrol.dart';
 import 'package:patrol_example/main.dart' as app;
 
@@ -8,6 +9,8 @@ void main() {
       app.main();
       await $('Native').tap();
       await $('Open Web View').tap();
+      await Future.delayed(Duration(seconds: 2));
+      await $.native2.swipe(from: Offset(0.6, 0.5), to: Offset(0.6, 0.45));
       await $.native2.tap(NativeSelector(
           android: AndroidSelector(text: 'ค้นหาหมวดหมู่งาน'), ios: IOSSelector(title: 'ค้นหาหมวดหมู่งาน')));
       await $.native2
